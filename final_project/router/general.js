ff5177be5e1c75e94f1e159bf04e5c116167b460
DharaniNavodya <e20350@eng.pdn.ac.lk>
2026-02-27 15:23:21 +0530

    Task 10-11: async/await axios for all books and ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,21 +20,32 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/',async function (req, res) {
   //Write your code here
-  return res.status(200).send(JSON.stringify(books,null,4));
+  try{
+    const getBookAsync=()=> new Promise((resolve)=>{resolve(books);
+  });
+  const allBooks=await getBookAsync();
+    return res.status(200).send(JSON.stringify(allBooks,null,4));
+  }catch(err){
+    return res.status(500).json({message:"Error retrieving books"});
+}
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  const isbn=req.params.isbn;
-  if(books[isbn]){
-    return res.status(200).send(JSON.stringify(books[isbn],null,4));
-  }else{
-  return res.status(404).json({message: "Books not found"});
-  }
- });
+public_users.get('/isbn/:isbn',async function (req, res) {
+    try {
+      const isbn = req.params.isbn;
+      const response = await axios.get("http://localhost:5000/");
+      const allBooks = response.data;
+      if (allBooks && allBooks[isbn]) {
+        return res.status(200).send(JSON.stringify(allBooks[isbn], null, 4));
+      }
+      return res.status(404).json({ message: "Book not found" });
+    } catch (e) {
+      return res.status(500).json({ message: "Error retrieving book by ISBN" });
+    }
+  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
